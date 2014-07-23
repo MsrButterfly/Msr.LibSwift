9bb641367dbe64753ee5dbaca3e7fe3a88739250
MSR-B <liuhongzhe19940501@gmail.com>
2014-07-23 16:31:12 +0800

    [UI] Added an extension for hiding keyboard.

---
+++ UI/UIView+KeyboardUtility.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+extension UIView {
+    func resignFirstResponderOfAllSubviews() -> Void {
+        for subview in subviews {
+            if let textView = subview as? UITextView {
+                textView.resignFirstResponder()
+            } else if let textField = subview as? UITextField {
+                textField.resignFirstResponder()
+            }
+            (subview as UIView).resignFirstResponderOfAllSubviews()
+        }
+    }
+}
