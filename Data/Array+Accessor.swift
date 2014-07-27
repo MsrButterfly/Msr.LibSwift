8dafa965a2c89bb371d8bf21bfb7e8673e4938bc
MSR-B <liuhongzhe19940501@gmail.com>
2014-07-27 16:29:17 +0800

    [Data] Removed useless accessors.

--- Data/Array+Accessor.swift
+++ Data/Array+Accessor.swift
@@ -1,24 +1,13 @@
 import Foundation
 
 extension Array {
-    var firstIndex: Array<T>.IndexType! {
-        get {
-            return count > 0 ? 0 : nil
-        }
+    var firstOne: T {
+        return self[startIndex]
     }
-    var firstOne: T! {
-        get {
-            return firstIndex ? self[firstIndex] : nil
-        }
-    }
-    var lastIndex: Array<T>.IndexType! {
-        get {
-            return count > 0 ? endIndex - 1 : nil
-        }
-    }
-    var lastOne: T! {
-        get {
-            return lastIndex ? self[lastIndex] : nil
-        }
+}
+
+extension Array {
+    var lastOne: T {
+        return self[endIndex - 1]
     }
 }
