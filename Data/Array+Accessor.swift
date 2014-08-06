705473751f1dac9c525d286e08ef99378010d9e9
MSR-B <liuhongzhe19940501@gmail.com>
2014-08-06 17:25:58 +0800

    [Data] Removed accessors added in new swift language.

--- Data/Array+Accessor.swift
+++ Data/Array+Accessor.swift
@@ -1,18 +1,6 @@
 import Foundation
 
 extension Array {
-    var firstOne: T {
-        return self[startIndex]
-    }
-}
-
-extension Array {
-    var lastOne: T {
-        return self[endIndex - 1]
-    }
-}
-
-extension Array {
     mutating func removeFirst() -> T {
         return removeAtIndex(startIndex)
     }
