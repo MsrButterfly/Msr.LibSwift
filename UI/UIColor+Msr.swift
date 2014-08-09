1fb5fb3ac8363997a1f643d6b516dcc75820471e
MSR-B <liuhongzhe19940501@gmail.com>
2014-08-09 22:17:51 +0800

    [UI] Added initializers to make creating colors more convenience.

--- UI/UIColor+Msr.swift
+++ UI/UIColor+Msr.swift
@@ -1,6 +1,20 @@
 import UIKit
 
 extension UIColor {
+    dynamic convenience init(RGBA: UInt32) {
+        var v = [CGFloat]()
+        var rgba = RGBA
+        for _ in 1...4 {
+            v.append(CGFloat(rgba % UInt32(0x100)) / CGFloat(0xff))
+            rgba >>= 8
+        }
+        self.init(red: v[3], green: v[2], blue: v[1], alpha: v[0])
+    }
+    dynamic convenience init(RGB: UInt32) {
+        var RGBA = UInt32(RGB) << UInt32(8)
+        RGBA += 0xff
+        self.init(RGBA: RGBA)
+    }
     class func randomColor(opaque: Bool) -> UIColor {
         let max = 255 as UInt32
         let red = CGFloat(Int(arc4random_uniform(max))) / CGFloat(255)
