e1d7eec4966fb1cb2f2b60baf97d86efa568de8c
Darren Liu <butterfly@msrlab.org>
2015-02-25 10:22:34 +0800

    [UI] Bug fixes.

--- UI/CGRect+Accessor.swift
+++ UI/CGRect+Accessor.swift
@@ -1,45 +1,49 @@
 import UIKit
 
 extension CGRect {
-    var msr_leftX: CGFloat {
+    var msr_left: CGFloat {
         set {
+            let right = msr_right
             origin.x = newValue
+            msr_right = right
         }
         get {
             return origin.x
         }
     }
-    var msr_rightX: CGFloat {
+    var msr_right: CGFloat {
         set {
-            size.width = newValue - origin.x
+            size.width = newValue - msr_left
         }
         get {
-            return origin.x + width
+            return msr_left + width
         }
     }
-    var msr_topY: CGFloat {
+    var msr_top: CGFloat {
         set {
+            let bottom = msr_bottom
             origin.y = newValue
+            msr_bottom = bottom
         }
         get {
             return origin.y
         }
     }
-    var msr_downY: CGFloat {
+    var msr_bottom: CGFloat {
         set {
-            size.height = newValue - origin.x
+            size.height = newValue - msr_top
         }
         get {
-            return origin.y + height
+            return msr_top + height
         }
     }
     var msr_center: CGPoint {
         set {
-            msr_leftX = newValue.x - width / 2
-            msr_topY = newValue.y - height / 2
+            msr_left = newValue.x - width / 2
+            msr_top = newValue.y - height / 2
         }
         get {
-            return CGPoint(x: msr_leftX + width / 2, y: msr_topY + height / 2)
+            return CGPoint(x: msr_left + width / 2, y: msr_top + height / 2)
         }
     }
 }
