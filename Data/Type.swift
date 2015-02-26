d454abf64fbef6a421bf5f07b28ce2301064842b
Darren Liu <butterfly@msrlab.org>
2015-02-27 01:06:32 +0800

    [Data] Bug fixes.

--- Data/Type.swift
+++ Data/Type.swift
@@ -1,20 +1,22 @@
 import Foundation
 
 extension Msr.Data {
-    static func PrintTypeTree(object: AnyObject, layer: Int = 0) -> Void {
+    static func DumpTypeTree(object: AnyObject, layer: Int = 1) -> Void {
         var indentationString = ""
-        for _ in 0...layer {
+        for _ in 0..<layer {
             indentationString += "   "
         }
+        println(String.fromCString(object_getClassName(object))!)
         if let data = object as? NSDictionary {
             for (key, value) in data {
-                print(indentationString + "\(key) ")
-                PrintTypeTree(value, layer: layer + 1)
+                print(indentationString + "\(key): ")
+                DumpTypeTree(value, layer: layer + 1)
             }
         } else if let data = object as? NSArray {
+            println("NSArray")
             for value in data {
                 print(indentationString)
-                PrintTypeTree(value, layer: layer + 1)
+                DumpTypeTree(value, layer: layer + 1)
             }
         }
     }
