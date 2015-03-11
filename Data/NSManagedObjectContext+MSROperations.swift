372700e29a4e0eab956d900dd821166890fa9b77
Darren Liu <butterfly@msrlab.org>
2015-03-11 11:40:06 +0800

    [Data] New file.

---
+++ Data/NSManagedObjectContext+MSROperations.swift
@@ -0,0 +1,23 @@
+import CoreData
+
+extension NSManagedObjectContext {
+    @objc func msr_deleteAllObjectsWithEntityName(entityName: String, error: NSErrorPointer) {
+        let request = NSFetchRequest(entityName: entityName)
+        request.predicate = NSPredicate(value: true)
+        let results = executeFetchRequest(request, error: error)
+        if results != nil {
+            for r in results! {
+                deleteObject(r as! NSManagedObject)
+            }
+        } else {
+            if error != nil {
+                error.memory = NSError() // Needs specification
+            }
+        }
+    }
+    @objc func msr_deleteAllObjectsWithEntityName(entityName: String) -> NSError? {
+        var error: NSError?
+        msr_deleteAllObjectsWithEntityName(entityName, error: &error)
+        return error
+    }
+}
