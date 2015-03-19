6a2e131516d96efd269cf35f5800ece2245c52eb
Darren Liu <butterfly@msrlab.org>
2015-03-20 00:25:47 +0800

    [Data] Made numeric type initializer extensions failable.

--- Data/Numeric+MSRInitializer.swift
+++ Data/Numeric+MSRInitializer.swift
@@ -1,35 +1,53 @@
 extension Int {
-    init(msr_object: AnyObject) {
-        self.init(NSString(string: "\(msr_object)").integerValue)
+    init?(msr_object: AnyObject?) {
+        if msr_object == nil {
+            return nil
+        }
+        self.init(NSString(string: "\(msr_object!)").integerValue)
     }
 }
 
 extension Int32 {
-    init(msr_object: AnyObject) {
-        self.init(NSString(string: "\(msr_object)").intValue)
+    init?(msr_object: AnyObject?) {
+        if msr_object == nil {
+            return nil
+        }
+        self.init(NSString(string: "\(msr_object!)").intValue)
     }
 }
 
 extension Int64 {
-    init(msr_object: AnyObject) {
-        self.init(NSString(string: "\(msr_object)").longLongValue)
+    init?(msr_object: AnyObject?) {
+        if msr_object == nil {
+            return nil
+        }
+        self.init(NSString(string: "\(msr_object!)").longLongValue)
     }
 }
 
 extension Float {
-    init(msr_object: AnyObject) {
-        self.init(NSString(string: "\(msr_object)").floatValue)
+    init?(msr_object: AnyObject?) {
+        if msr_object == nil {
+            return nil
+        }
+        self.init(NSString(string: "\(msr_object!)").floatValue)
     }
 }
 
 extension Double {
-    init(msr_object: AnyObject) {
-        self.init(NSString(string: "\(msr_object)").doubleValue)
+    init?(msr_object: AnyObject?) {
+        if msr_object == nil {
+            return nil
+        }
+        self.init(NSString(string: "\(msr_object!)").doubleValue)
     }
 }
 
 extension Bool {
-    init(msr_object: AnyObject) {
-        self.init(NSString(string: "\(msr_object)").boolValue)
+    init?(msr_object: AnyObject?) {
+        if msr_object == nil {
+            return nil
+        }
+        self.init(NSString(string: "\(msr_object!)").boolValue)
     }
 }
