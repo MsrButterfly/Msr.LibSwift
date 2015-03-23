8a92aa7902a15eb4d5a17c59ab55424a87b1ee03
Darren Liu <butterfly@msrlab.org>
2015-03-23 13:59:34 +0800

    [UI] New file.

---
+++ UI/MSRCollectionView.swift
@@ -0,0 +1,15 @@
+class MSRCollectionView: UICollectionView {
+    
+    override var contentSize: CGSize {
+        didSet {
+            if contentSize != oldValue {
+                invalidateIntrinsicContentSize()
+            }
+        }
+    }
+    
+    override func intrinsicContentSize() -> CGSize {
+        return contentSize
+    }
+    
+}
