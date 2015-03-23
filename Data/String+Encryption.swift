e21366e57e33c55626803197b49ead03eb76266d
Darren Liu <butterfly@msrlab.org>
2015-03-23 13:59:08 +0800

    [Data] New file.

---
+++ Data/String+Encryption.swift
@@ -0,0 +1,14 @@
+extension String {
+    var msr_MD5EncryptedString: String {
+        let cStr = NSString(string: self).UTF8String
+        let length = Int(CC_MD5_DIGEST_LENGTH)
+        let buffer = UnsafeMutablePointer<CUnsignedChar>.alloc(length)
+        CC_MD5(cStr, CC_LONG(strlen(cStr)), buffer)
+        var result = ""
+        for i in 0..<length {
+            result += NSString(format: "%02X", buffer.advancedBy(i).memory) as String
+        }
+        free(buffer)
+        return result
+    }
+}
