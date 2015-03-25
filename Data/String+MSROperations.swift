67c4aed9d0cfc49903d9af7b8250021dc042fb2a
Darren Liu <butterfly@msrlab.org>
2015-03-25 15:26:56 +0800

    [Data] New file.

---
+++ Data/String+MSROperations.swift
@@ -0,0 +1,5 @@
+extension String {
+    func msr_stringByRemovingCharactersInSet(set: NSCharacterSet) -> String {
+        return (componentsSeparatedByCharactersInSet(set) as NSArray).componentsJoinedByString("")
+    }
+}
