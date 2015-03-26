d8bac3d5618e6f1045031b5820b2c1dc46a7b3d2
Darren Liu <butterfly@msrlab.org>
2015-03-26 10:39:21 +0800

    [UI] Removed useless function call.

--- UI/MSRMultilineLabel.swift
+++ UI/MSRMultilineLabel.swift
@@ -1,6 +1,5 @@
 class MSRMultilineLabel: UILabel {
     override func layoutSubviews() {
-        super.layoutSubviews()
         preferredMaxLayoutWidth = bounds.width
         super.layoutSubviews()
     }
