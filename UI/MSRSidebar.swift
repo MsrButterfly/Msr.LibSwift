96c0e2d7eae0a0bec217035a92637d85c32638ba
Darren Liu <butterfly@msrlab.org>
2015-03-30 15:06:24 +0800

    [UI] MSRSidebar was redesigned.

--- UI/MSRSidebar.swift
+++ UI/MSRSidebar.swift
@@ -1,165 +1,254 @@
-@objc class MSRSidebar: UIView {
-    let contentView: UIView
-    let backgroundEffect: UIBlurEffect
-    private var handle: _MSRSidebarHandle!
-    let offset: CGFloat
-    let width: CGFloat
-    let overlay: UIView
-    let overlayMaxAlpha = CGFloat(0.5)
-    var overlayPanGestureRecognizer: UIPanGestureRecognizer!
-    var overlayTapGestureRecognizer: UITapGestureRecognizer!
-    override var hidden: Bool {
-        get {
-            return frame.origin.x != -offset
-        }
-        set(value) {
-            if value {
-                hide(animated: false, completion: nil)
-            } else {
-                show(animated: false, completion: nil)
-            }
-        }
-    }
-    init(width: CGFloat, blurEffectStyle: UIBlurEffectStyle) {
-        contentView = UIView()
-        overlay = UIView()
-        backgroundEffect = UIBlurEffect(style: blurEffectStyle)
+/*
+
+Functional Synopsis
+
+class MSRSidebar: UIView, UIGestureRecognizerDelegate {
+
+    init(width: CGFloat, edge: MSRFrameEdge)
+
+    var backgroundView: UIView?
+    var collapsed: Bool
+    var contentView: UIView
+    var edge: MSRFrameEdge { get }
+    var overlay: UIView?
+    var overlayPanGestureRecognizer: UIPanGestureRecognizer { get }
+    var overlayTapGestureRecognizer: UITapGestureRecognizer { get }
+    var screenEdgePanGestureRecognizer: UIScreenEdgePanGestureRecognizer { get }
+    var width: CGFloat
+
+    func collapse()
+    func collapse(#animated: Bool)
+    func setCollapsed(collapsed: Bool, animated: Bool)
+    func expand()
+    func expand(#animated: Bool)
+
+*/
+
+import UIKit
+
+class MSRSidebar: UIView, UIGestureRecognizerDelegate {
+    // MARK: - Initializers
+    init(width: CGFloat, edge: MSRFrameEdge) {
+        self.edge = edge
         self.width = width
-        let handleWidth = CGFloat(12)
-        offset = UIScreen.mainScreen().bounds.width - width + handleWidth
-        var frame = UIScreen.mainScreen().bounds
-        frame.size.width *= 2
-        frame.size.width += handleWidth
-        frame.origin.x = -(offset + width)
-        super.init(frame: frame)
-        let backgroundView = UIVisualEffectView(effect: backgroundEffect)
-        frame = UIScreen.mainScreen().bounds
-        frame.size.width += handleWidth
-        backgroundView.frame = frame
-        addSubview(backgroundView)
-        handle = _MSRSidebarHandle(sidebar: self, width: handleWidth)
-        let vibrancyEffectView = UIVisualEffectView(effect: UIVibrancyEffect(forBlurEffect: backgroundView.effect as! UIBlurEffect))
-        vibrancyEffectView.frame = bounds
-        backgroundView.contentView.addSubview(vibrancyEffectView)
-        vibrancyEffectView.contentView.addSubview(handle)
-        contentView.frame = CGRect(x: offset, y: 0, width: width - handle.bounds.width, height: backgroundView.bounds.height)
-        backgroundView.contentView.addSubview(contentView)
-        frame = bounds
-        frame.origin.x = backgroundView.bounds.width
-        frame.size.width = UIScreen.mainScreen().bounds.width
-        if blurEffectStyle == .Dark {
-            overlay.backgroundColor = UIColor.clearColor()
-        } else {
-            overlay.backgroundColor = UIColor.blackColor()
-        }
-        overlay.frame = frame
-        overlay.alpha = 0
-        addSubview(overlay)
-        overlayPanGestureRecognizer = UIPanGestureRecognizer(target: self, action: "pan:")
-        overlay.addGestureRecognizer(overlayPanGestureRecognizer)
-        overlayTapGestureRecognizer = UITapGestureRecognizer(target: self, action: "tap:")
-        overlay.addGestureRecognizer(overlayTapGestureRecognizer)
+        super.init(frame: CGRectZero)
+        msr_initialize()
     }
     required init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        edge = .Left
+        width = 0
+        super.init(coder: aDecoder)
+        msr_initialize()
     }
-    override func hitTest(point: CGPoint, withEvent event: UIEvent!) -> UIView? {
-        if let view = handle.hitTest(point, withEvent: event) {
-            return view
-        }
-        return super.hitTest(point, withEvent: event)
+    override init(frame: CGRect) {
+        edge = .Left
+        width = frame.width
+        super.init(frame: frame)
+        msr_initialize()
     }
-    func show(#animated: Bool) {
-        show(animated: animated, completion: nil)
+    func msr_initialize() {
+        backgroundView = UIVisualEffectView(effect: UIBlurEffect(style: .Dark))
+        msr_shouldTranslateAutoresizingMaskIntoConstraints = false
+        collapsed = true
+        addSubview(contentView)
+        contentView.msr_addVerticalEdgeAttachedConstraintsToSuperview()
+        contentView.msr_addEdgeAttachedConstraintToSuperviewAtEdge(edge == .Left ? .Right : .Left)
+        contentView.addConstraint(contentViewWidthConstraint)
     }
-    func show(#animated: Bool, completion: ((Bool) -> Void)?) {
-        let animations: () -> Void = {
-            [weak self] in
-            var frame = self!.frame
-            frame.origin.x = -self!.offset
-            self!.frame = frame
-            self!.overlay.alpha = self!.overlayMaxAlpha
-        }
-        if animated {
-            UIView.animateWithDuration(0.3,
-                delay: 0,
-                usingSpringWithDamping: 1,
-                initialSpringVelocity: 0,
-                options: UIViewAnimationOptions.BeginFromCurrentState,
-                animations: animations,
-                completion: completion)
-        } else {
-            animations()
-        }
-    }
-    func hide(#animated: Bool) {
-        hide(animated: animated, completion: nil)
-    }
-    func hide(#animated: Bool, completion: ((Bool) -> Void)?) {
-        let animations: () -> Void = {
-            [weak self] in
-            var frame = self!.frame
-            frame.origin.x = -(UIScreen.mainScreen().bounds.width + self!.handle.bounds.width)
-            self!.frame = frame
-            self!.overlay.alpha = 0
-        }
-        if animated {
-            UIView.animateWithDuration(0.3,
-                delay: 0,
-                usingSpringWithDamping: 1,
-                initialSpringVelocity: 0,
-                options: UIViewAnimationOptions.BeginFromCurrentState,
-                animations: animations,
-                completion: completion)
-        } else {
-            animations()
-        }
-    }
-    internal func pan(gestureRecognizer: UIGestureRecognizer?) {
-        if let recognizer = gestureRecognizer as? UIPanGestureRecognizer {
-            let offset = recognizer.locationInView(handle).x
-            switch recognizer.state {
-            case .Began:
-                handle.pan(recognizer)
-                break
-            case .Changed:
-                handle.pan(recognizer)
-                break
-            case .Ended:
-                handle.pan(recognizer)
-            default:
-                break
+    // MARK: - Variables
+    var backgroundView: UIView? {
+        didSet {
+            oldValue?.removeFromSuperview()
+            if backgroundView != nil {
+                backgroundView!.autoresizingMask = .FlexibleWidth | .FlexibleHeight
+                backgroundView!.bounds = bounds
+                backgroundView!.center = center
+                insertSubview(backgroundView!, belowSubview: contentView)
             }
         }
     }
-    internal func tap(gestureRecognizer: UIGestureRecognizer?) {
-        if let recognizer = gestureRecognizer as? UITapGestureRecognizer {
-            hide(animated: true, completion: nil)
+    var collapsed: Bool = true {
+        didSet {
+            updateUIWithValue(collapsed ? 0 : width)
         }
     }
-    internal func offsetForTouchPoint(point: CGPoint) -> CGFloat {
-        if point.x < width {
-            return point.x - (UIScreen.mainScreen().bounds.width + handle.bounds.width)
+    lazy var contentView: UIView = {
+        let v = UIView()
+        v.msr_shouldTranslateAutoresizingMaskIntoConstraints = false
+        return v
+    }()
+    let edge: MSRFrameEdge
+    var overlay: UIView? {
+        didSet {
+            oldValue?.removeFromSuperview()
+            if overlay != nil {
+                overlay!.autoresizingMask = .FlexibleWidth | .FlexibleHeight
+                overlay!.bounds = overlayWrapper.bounds
+                overlay!.center = overlayWrapper.center
+                overlayWrapper.addSubview(overlay!)
+            }
         }
-        return 0.5 * (point.x + width) - (UIScreen.mainScreen().bounds.width + handle.bounds.width)
     }
-    internal func alphaForTouchPoint(point: CGPoint) -> CGFloat {
-        if point.x < width {
-            return point.x / width * overlayMaxAlpha
+    private(set) lazy var overlayPanGestureRecognizer: UIPanGestureRecognizer = {
+        [weak self] in
+        let recognizer = UIPanGestureRecognizer(target: self!, action: "handleOverlayPanGesture:")
+        return recognizer
+    }()
+    private(set) lazy var overlayTapGestureRecognizer: UITapGestureRecognizer = {
+        [weak self] in
+        let recognizer = UITapGestureRecognizer(target: self!, action: "handleOverlayTapGesture:")
+        return recognizer
+    }()
+    private(set) lazy var screenEdgePanGestureRecognizer: UIScreenEdgePanGestureRecognizer = {
+        [weak self] in
+        let recognizer = UIScreenEdgePanGestureRecognizer(target: self!, action: "handleScreenEdgePanGesture:")
+        recognizer.delegate = self
+        recognizer.edges = self!.edge == .Left ? .Left : .Right
+        recognizer.delaysTouchesBegan = true
+        return recognizer
+    }()
+    var width: CGFloat {
+        didSet {
+            widthConstraint?.constant = width
+            contentViewWidthConstraint.constant = width
+            layoutIfNeeded()
         }
-        return overlayMaxAlpha
     }
-}
-
-extension MSRSidebar {
-    func toggleShow(#animated: Bool) {
-        toggleShow(animated: animated, completion: nil)
+    // MARK: - Methods
+    func collapse() {
+        collapse(animated: true)
     }
-    func toggleShow(#animated: Bool, completion: ((Bool) -> Void)?) {
-        if hidden {
-            show(animated: animated, completion: completion)
+    func collapse(#animated: Bool) {
+        setCollapsed(true, animated: animated)
+    }
+    func setCollapsed(collapsed: Bool, animated: Bool) {
+        let animations: () -> Void = {
+            [weak self] in
+            self?.collapsed = collapsed
+            return
+        }
+        if animated {
+            UIView.animateWithDuration(0.5,
+                delay: 0,
+                usingSpringWithDamping: collapsed ? 1 : 0.5,
+                initialSpringVelocity: 0.7,
+                options: .BeginFromCurrentState,
+                animations: animations,
+                completion: nil)
         } else {
-            hide(animated: animated, completion: completion)
+            animations()
         }
+    }
+    func expand() {
+        expand(animated: true)
+    }
+    func expand(#animated: Bool) {
+        setCollapsed(false, animated: animated)
+    }
+    // MARK: - Override Methods
+    override func willMoveToSuperview(newSuperview: UIView?) {
+        if superview != nil {
+            overlayWrapper.removeFromSuperview()
+            overlayWrapper.msr_removeAllEdgeAttachedConstraintsFromSuperview()
+            msr_removeVerticalEdgeAttachedConstraintsFromSuperview()
+            edgeConstraint = nil
+            widthConstraint = nil
+            overlayWrapper.removeGestureRecognizer(overlayPanGestureRecognizer)
+            overlayWrapper.removeGestureRecognizer(overlayTapGestureRecognizer)
+            superview!.removeGestureRecognizer(screenEdgePanGestureRecognizer)
+        }
+    }
+    override func didMoveToSuperview() {
+        if superview != nil {
+            superview!.insertSubview(overlayWrapper, belowSubview: self)
+            overlayWrapper.msr_addVerticalEdgeAttachedConstraintsToSuperview()
+            overlayWrapper.msr_addEdgeAttachedConstraintToSuperviewAtEdge(edge == .Left ? .Right : .Left)
+            
+            msr_addVerticalEdgeAttachedConstraintsToSuperview()
+            edgeConstraint = NSLayoutConstraint(item: self, attribute: edge == .Left ? .Right : .Left, relatedBy: .Equal, toItem: superview, attribute: edge == .Left ? .Left : .Right, multiplier: 1, constant: 0)
+            widthConstraint = NSLayoutConstraint(item: self, attribute: .Width, relatedBy: .Equal, toItem: superview, attribute: .Width, multiplier: 1, constant: width)
+            superview!.addConstraint(edgeConstraint!)
+            superview!.addConstraint(widthConstraint!)
+            superview!.addConstraint(NSLayoutConstraint(item: self, attribute: edge == .Left ? .Right : .Left, relatedBy: .Equal, toItem: overlayWrapper, attribute: edge == .Left ? .Left : .Right, multiplier: 1, constant: 0))
+            overlayWrapper.addGestureRecognizer(overlayPanGestureRecognizer)
+            overlayWrapper.addGestureRecognizer(overlayTapGestureRecognizer)
+            superview!.addGestureRecognizer(screenEdgePanGestureRecognizer)
+        }
+    }
+    override func gestureRecognizerShouldBegin(gestureRecognizer: UIGestureRecognizer) -> Bool {
+        if gestureRecognizer === screenEdgePanGestureRecognizer {
+            return collapsed
+        }
+        return super.gestureRecognizerShouldBegin(gestureRecognizer)
+    }
+    // MARK: - Private Variables
+    private lazy var contentViewWidthConstraint: NSLayoutConstraint = {
+        [weak self] in
+        return NSLayoutConstraint(item: self!.contentView, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: self!.width)
+        }()
+    private var edgeConstraint: NSLayoutConstraint?
+    private lazy var overlayWrapper: UIView = {
+        let v = UIView()
+        v.msr_shouldTranslateAutoresizingMaskIntoConstraints = false
+        return v
+    }()
+    private var widthConstraint: NSLayoutConstraint?
+    // MARK: - Private Methods
+    internal func handleScreenEdgePanGesture(recognizer: UIScreenEdgePanGestureRecognizer) {
+        if recognizer === screenEdgePanGestureRecognizer {
+            updateTranslationByHandlingPanGesture(recognizer)
+        }
+    }
+    internal func handleOverlayPanGesture(recognizer: UIPanGestureRecognizer) {
+        if recognizer === overlayPanGestureRecognizer {
+            updateTranslationByHandlingPanGesture(recognizer)
+        }
+    }
+    internal func handleOverlayTapGesture(recognizer: UITapGestureRecognizer) {
+        if recognizer === overlayTapGestureRecognizer {
+            collapse(animated: true)
+        }
+    }
+    private func updateTranslationByHandlingPanGesture(recognizer: UIPanGestureRecognizer) {
+        var location = recognizer.locationInView(superview!).x
+        var velocity = recognizer.velocityInView(superview!).x
+        if edge == .Right {
+            location = superview!.bounds.width - location
+            velocity = -velocity
+        }
+        switch recognizer.state {
+        case .Began:
+            UIView.animateWithDuration(0.1,
+                delay: 0,
+                usingSpringWithDamping: 1,
+                initialSpringVelocity: 0.7,
+                options: .BeginFromCurrentState,
+                animations: {
+                    [weak self] in
+                    self?.updateUIWithValue(location)
+                    return
+                },
+                completion: nil)
+            break
+        case .Changed:
+            updateUIWithValue(location)
+            break
+        case .Cancelled, .Ended:
+            setCollapsed(velocity < 0, animated: true)
+            break
+        default:
+            break
+        }
+    }
+    private func updateUIWithValue(value: CGFloat) {
+        let translation = value <= width ? value : ((value + width) / 2)
+        edgeConstraint?.constant = edge == .Left ? translation : -translation
+        overlayWrapper.alpha = min(max(translation / width, 0), 1)
+        overlayWrapper.layoutIfNeeded()
+        layoutIfNeeded()
+    }
+    // MARK: Deinitializer
+    deinit {
+        screenEdgePanGestureRecognizer.delegate = nil
     }
 }
