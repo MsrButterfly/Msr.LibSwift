4070a5e96a5ce8887234607bc4c96fecdc96d671
Darren Liu <butterfly@msrlab.org>
2015-05-14 15:57:13 +0800

    [UI] Added some convenient methods for MSRAnimationInfo.

--- UI/MSRAnimationInfo.swift
+++ UI/MSRAnimationInfo.swift
@@ -8,12 +8,25 @@ import UIKit
         let info = keyboardNotification.userInfo! as! [NSString: AnyObject]
         frameBegin = info[UIKeyboardFrameBeginUserInfoKey]!.CGRectValue()
         frameEnd = info[UIKeyboardFrameEndUserInfoKey]!.CGRectValue()
-        animationDuration = info[UIKeyboardAnimationDurationUserInfoKey]!.doubleValue!
-        animationCurve = UIViewAnimationCurve(rawValue: info[UIKeyboardAnimationCurveUserInfoKey]!.integerValue!)!
+        duration = info[UIKeyboardAnimationDurationUserInfoKey]!.doubleValue!
+        curve = UIViewAnimationCurve(rawValue: info[UIKeyboardAnimationCurveUserInfoKey]!.integerValue!)!
         super.init()
+    }
+    func animate(animation: (() -> Void)) {
+        animate(animation, completion: nil)
+    }
+    func animate(animation: (() -> Void), completion: ((Bool) -> Void)?) {
+        animate(delay: 0, options: nil, animation: animation, completion: completion)
+    }
+    func animate(#delay: NSTimeInterval, options: UIViewAnimationOptions, animation: (() -> Void), completion: ((Bool) -> Void)?) {
+        UIView.animateWithDuration(duration,
+            delay: delay,
+            options: UIViewAnimationOptions(rawValue: UInt(curve.rawValue)) | options,
+            animations: animation,
+            completion: completion)
     }
     var frameBegin: CGRect = CGRectZero
     var frameEnd: CGRect = CGRectZero
-    var animationDuration: NSTimeInterval = 0
-    var animationCurve: UIViewAnimationCurve = .Linear
+    var duration: NSTimeInterval = 0
+    var curve: UIViewAnimationCurve = .Linear
 }
