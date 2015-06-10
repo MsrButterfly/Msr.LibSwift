b1989346cda2067e115f338af7c9ad0bd468f035
Darren Liu <butterfly@msrlab.org>
2015-06-11 03:09:06 +0800

    [UI] Added getter for UINavigationBar shadow image view.

--- UI/UINavigationBar+MSRAccessor.swift
+++ UI/UINavigationBar+MSRAccessor.swift
@@ -2,9 +2,9 @@ import UIKit
 
 extension UINavigationBar {
     @objc var msr_backgroundView: UIImageView? {
-        return subviews.filter({
-            (v: AnyObject) in
-            NSStringFromClass(v.dynamicType) == "_UINavigationBarBackground"
-        }).first as? UIImageView
+        return valueForKey("_backgroundView") as? UIImageView
+    }
+    @objc var msr_shadowImageView: UIImageView? {
+        return msr_backgroundView?.valueForKey("_shadowView") as? UIImageView
     }
 }
