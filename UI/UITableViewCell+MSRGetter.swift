7a4adebd80ecb3a0ea820f6d4c7f5d4329225b54
Darren Liu <butterfly@msrlab.org>
2016-06-15 17:53:22 +0800

    [UI] Removed some version requirements in UITableViewCell.msr_scrollView.

--- UI/UITableViewCell+MSRGetter.swift
+++ UI/UITableViewCell+MSRGetter.swift
@@ -3,7 +3,7 @@ import UIKit
 extension UITableViewCell {
     var msr_scrollView: UIScrollView? {
         let systemVersion = (UIDevice.currentDevice().systemVersion as NSString).floatValue
-        if systemVersion < 7.0 || systemVersion >= 8.0 {
+        if systemVersion < 7.0 {
             return nil
         }
         for view in subviews {
