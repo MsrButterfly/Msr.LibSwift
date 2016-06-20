ff310bb4ab041decdd7980a8eab3d1937d155598
Darren Liu <butterfly@msrlab.org>
2016-06-20 17:22:19 +0800

    [Data] Added SHA1 encryption extension for String.

--- Data/String+MSREncryption.swift
+++ Data/String+MSREncryption.swift
@@ -2,8 +2,9 @@ import Foundation
 import CommonCrypto
 
 extension String {
+    
     var msr_MD5EncryptedString: String {
-        let cStr = NSString(string: self).UTF8String
+        let cStr = (self as NSString).UTF8String
         let length = Int(CC_MD5_DIGEST_LENGTH)
         let buffer = UnsafeMutablePointer<CUnsignedChar>.alloc(length)
         CC_MD5(cStr, CC_LONG(strlen(cStr)), buffer)
@@ -14,4 +15,17 @@ extension String {
         free(buffer)
         return result
     }
+    
+    var msr_SHA1EncryptedString: String {
+        let cStr = (self as NSString).UTF8String
+        let length = Int(CC_SHA1_DIGEST_LENGTH)
+        let buffer = UnsafeMutablePointer<CUnsignedChar>.alloc(length)
+        CC_SHA1(cStr, CC_LONG(strlen(cStr)), buffer)
+        var result = ""
+        for i in 0..<length {
+            result += NSString(format: "%02x", buffer.advancedBy(i).memory) as String
+        }
+        return result
+    }
+    
 }
